7fde00b1ac30a5837e984452f4ee69bbc08b1914
Luisa Tenorio <luisatenorio@Luisas-MacBook-Pro.local>
2025-02-22 13:32:08 +0100

    added setting form

--- src/app/views/settings/settings.page.ts
+++ src/app/views/settings/settings.page.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { GameService } from '../../services/game.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './settings.page.html',
-  styleUrl: './settings.page.scss'
+  styleUrl: './settings.page.scss',
 })
-export class SettingsPage {
+export class SettingsPage implements OnInit {
+  setUpForm!: FormGroup;
 
+  constructor(private fb: FormBuilder, private router: Router) {}
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm() {
+    this.setUpForm = this.fb.group({
+      cells: [, [Validators.required]],
+      wells: [, [Validators.required]],
+      arrows: [, [Validators.required]],
+    });
+  }
+
+  next() {
+    this.router.navigate(['/game']);
+  }
 }
