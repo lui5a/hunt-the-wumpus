f00038e9e1404908ca8d572de62cd79c3d3d8027
Luisa Tenorio <luisatenorio@Luisas-MacBook-Pro.local>
2025-02-22 13:21:12 +0100

    added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { GamePage } from './views/game/game.page';
+import { SettingsPage } from './views/settings/settings.page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'settings', component: SettingsPage },
+  { path: 'game', component: GamePage },
+  { path: '', redirectTo: '/settings', pathMatch: 'full' },
+];
