a6801bd78bb3e81e8d8b5e297dbf10cd2abf5604
Luisa Tenorio <luisatenorio@Luisas-MacBook-Pro.local>
2025-02-22 20:20:45 +0100

    added guard for routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { GamePage } from './views/game/game.page';
 import { SettingsPage } from './views/settings/settings.page';
+import { SettingsGuard } from './guards/settings.guard';
 
 export const routes: Routes = [
   { path: 'settings', component: SettingsPage },
-  { path: 'game', component: GamePage },
+  { path: 'game', component: GamePage, canActivate: [SettingsGuard] },
   { path: '', redirectTo: '/settings', pathMatch: 'full' },
 ];
