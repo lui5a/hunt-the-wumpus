9375366f481124c81cbd86cf69d2a29aa3fde8fc
Luisa Tenorio <luisatenorio@Luisas-MacBook-Pro.local>
2025-02-22 20:21:36 +0100

    added markup for board and placed items enums on cells

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,12 +1,71 @@
-import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { GameService } from '../../services/game.service';
 
+enum BoardElement {
+  Empty = 0,
+  Hunter = 1,
+  Well = 2,
+  Wall = 3,
+  Wumpus = 4,
+  Gold = 5,
+  HunterWithGold = 6,
+}
 @Component({
   selector: 'app-board',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './board.component.html',
   styleUrl: './board.component.scss',
 })
-export class BoardComponent {
+export class BoardComponent implements OnInit {
   @Input() boardSize!: number;
+  board: number[][] = [];
+  hunterPosition = { row: 0, col: 0 };
+  wallQuantity: number = this.boardSize;
+  wumpusQuantity: number = 1;
+  goldQuantity: number = 1;
+
+  constructor(private gameService: GameService) {}
+
+  ngOnInit(): void {
+    this.initializeBoard();
+  }
+  initializeBoard() {
+    const wellsQuantity = this.gameService.getWells();
+    const arrowsQuantity = this.gameService.getArrows();
+
+    for (let i = 0; i < this.boardSize; i++) {
+      this.board[i] = [];
+      for (let j = 0; j < this.boardSize; j++) {
+        this.board[i][j] = 0;
+      }
+    }
+    this.hunterPosition = { row: this.boardSize - 1, col: 0 };
+    this.board[this.hunterPosition.row][this.hunterPosition.col] =
+      BoardElement.Hunter;
+
+    this.placeElements(wellsQuantity, BoardElement.Well);
+    this.placeElements(this.wallQuantity, BoardElement.Wall);
+    this.placeElements(this.wumpusQuantity, BoardElement.Wumpus);
+    this.placeElements(this.goldQuantity, BoardElement.Gold);
+  }
+
+  placeElements(count: number, type: number) {
+    let availableCells = [];
+
+    for (let row = 0; row < this.boardSize; row++) {
+      for (let col = 0; col < this.boardSize; col++) {
+        if (!(row === this.boardSize - 1 && col === 0)) {
+          availableCells.push({ row, col });
+        }
+      }
+    }
+    console.log(availableCells);
+    availableCells = availableCells.sort(() => Math.random() - 0.5);
+    for (let i = 0; i < count && i < availableCells.length; i++) {
+      const { row, col } = availableCells[i];
+      this.board[row][col] = type;
+    }
+  }
 }
