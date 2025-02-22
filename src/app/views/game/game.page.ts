e67ea3abe7251797937ecdf0cd4efafa3f016e38
Luisa Tenorio <luisatenorio@Luisas-MacBook-Pro.local>
2025-02-22 13:19:20 +0100

    added project folder structure

---
+++ src/app/views/game/game.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game',
+  standalone: true,
+  imports: [],
+  templateUrl: './game.page.html',
+  styleUrl: './game.page.scss'
+})
+export class GamePage {
+
+}
---
+++ src/app/views/settings/settings.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-settings',
+  standalone: true,
+  imports: [],
+  templateUrl: './settings.page.html',
+  styleUrl: './settings.page.scss'
+})
+export class SettingsPage {
+
+}
