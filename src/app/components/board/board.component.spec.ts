9905a08aa15d22dbc895fea0b037b98a00599b5e
Luisa Tenorio <luisatenorio@Luisas-MacBook-Pro.local>
2025-02-23 19:39:33 +0100

    added testing

--- src/app/components/board/board.component.spec.ts
+++ src/app/components/board/board.component.spec.ts
@@ -1,23 +1,49 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BoardComponent } from './board.component';
+import { GameService } from '../../services/game.service';
 
 describe('BoardComponent', () => {
   let component: BoardComponent;
   let fixture: ComponentFixture<BoardComponent>;
+  let gameServiceMock: jasmine.SpyObj<GameService>;
 
   beforeEach(async () => {
+    gameServiceMock = jasmine.createSpyObj('GameService', [
+      'getWells',
+      'getArrows',
+    ]);
+    gameServiceMock.getWells.and.returnValue(3);
+    gameServiceMock.getArrows.and.returnValue(5);
     await TestBed.configureTestingModule({
-      imports: [BoardComponent]
-    })
-    .compileComponents();
+      imports: [BoardComponent],
+      providers: [{ provide: GameService, useValue: gameServiceMock }],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(BoardComponent);
     component = fixture.componentInstance;
+    component.boardSize = 4;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call gameService methods', () => {
+    expect(gameServiceMock.getWells).toHaveBeenCalled();
+    expect(gameServiceMock.getArrows).toHaveBeenCalled();
+  });
+
+  it('should initialize the board with the correct size', () => {
+    expect(component.board.length).toBe(component.boardSize);
+    expect(component.board[0].length).toBe(component.boardSize);
+  });
+
+  it('should place the hunter in the starting position', () => {
+    expect(component.hunterPosition).toEqual({
+      row: component.boardSize - 1,
+      col: 0,
+    });
   });
 });
--- src/app/views/game/game.page.spec.ts
+++ src/app/views/game/game.page.spec.ts
@@ -1,16 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GamePage } from './game.page';
+import { GameService } from '../../services/game.service';
 
 describe('GamePage', () => {
   let component: GamePage;
   let fixture: ComponentFixture<GamePage>;
+  let gameServiceMock: jasmine.SpyObj<GameService>;
 
   beforeEach(async () => {
+    gameServiceMock = jasmine.createSpyObj('GameService', [
+      'getBoardSize',
+      'getWells',
+      'getArrows',
+    ]);
+    gameServiceMock.getBoardSize.and.returnValue(5);
+    gameServiceMock.getWells.and.returnValue(1);
+    gameServiceMock.getArrows.and.returnValue(3);
     await TestBed.configureTestingModule({
-      imports: [GamePage]
-    })
-    .compileComponents();
+      imports: [GamePage],
+      providers: [{ provide: GameService, useValue: gameServiceMock }],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(GamePage);
     component = fixture.componentInstance;
@@ -19,5 +29,13 @@ describe('GamePage', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize boardSize correctly', () => {
+    expect(component.boardSize).toBe(5);
+  });
+
+  it('should call gameService methods during initialization', () => {
+    expect(gameServiceMock.getBoardSize).toHaveBeenCalled();
   });
 });
